4b225bcf304cca0095d3772f4bac4e2f59e06e9c
Frank Albenesius <frankalbenesius@gmail.com>
2019-11-18 23:12:55 -0600

    Fetch squares data.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useSWR from "swr";
+import fetch from "unfetch";
+import "./App.css";
+
+const fetcher = url => fetch(url).then(r => r.json());
 
 function App() {
+  const { data: squares } = useSWR(
+    "https://poster-api.frank.dev/squares",
+    fetcher
+  );
+
+  console.log(squares);
+
+  if (!squares) {
+    return null;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {squares.map(square => (
+        <div key={square.loc}>
+          {square.loc} | {square.recipient}
+        </div>
+      ))}
     </div>
   );
 }

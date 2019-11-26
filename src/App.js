d8455b92b0227d6ebbf77b5e14ebdcea7cd604e2
Frank Albenesius <frankalbenesius@gmail.com>
2019-11-25 22:21:01 -0600

    Add arrow buttons.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import Layout from "./components/Layout";
 import Header from "./components/Header";
 import useSWR from "swr";
 import SquareViewer from "./components/SquareViewer";
+
+export const cells = {
+  x: 8,
+  y: 9
+};
 
 export default function App() {
   const [state, setState] = useState({
     location: getRandomLocation(),
     passphrase: ""
   });
-
-  useInterval(() => {
-    setState({
-      ...state,
-      location: getRandomLocation()
-    });
-  }, 2000);
 
   const { data: squares } = useSWR(
     "https://poster-api.frank.dev/squares",
@@ -29,35 +27,24 @@ export default function App() {
   return (
     <Layout>
       <Header>Collaborative Holliday Poster 2019</Header>
-      <SquareViewer squares={squares} location={state.location} />
+      <SquareViewer
+        squares={squares}
+        location={state.location}
+        onLocationChange={location => {
+          setState({
+            ...state,
+            location
+          });
+        }}
+      />
       <div>buttons</div>
     </Layout>
   );
 }
 
 const getRandomLocation = () => [
-  Math.floor(Math.random() * 8),
-  Math.floor(Math.random() * 9)
+  Math.floor(Math.random() * cells.x),
+  Math.floor(Math.random() * cells.y)
 ];
 
 const fetcher = url => fetch(url).then(r => r.json());
-
-function useInterval(callback, delay) {
-  const savedCallback = useRef();
-
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
-}
--- src/components/SquareViewer.js
+++ src/components/SquareViewer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Square from "./Square";
+import { cells } from "../App";
 
 export default function SquareViewer(props) {
   return (
@@ -12,8 +13,144 @@ export default function SquareViewer(props) {
         </PosterPositioner>
       </SquareFrame>
       <AdjacentSquareDimmer />
+      <ArrowControls
+        location={props.location}
+        onLocationChange={props.onLocationChange}
+      />
     </PosterFrame>
   );
+}
+
+function ArrowControls(props) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%"
+      }}
+    >
+      <div style={{ paddingTop: "100%" }}></div>
+      <UpArrow
+        location={props.location}
+        onLocationChange={props.onLocationChange}
+      />
+      <DownArrow
+        location={props.location}
+        onLocationChange={props.onLocationChange}
+      />
+      <RightArrow
+        location={props.location}
+        onLocationChange={props.onLocationChange}
+      />
+      <LeftArrow
+        location={props.location}
+        onLocationChange={props.onLocationChange}
+      />
+    </div>
+  );
+}
+
+const arrowStyles = {
+  position: "absolute",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "white",
+  fontSize: "2em"
+};
+function UpArrow(props) {
+  const [x, y] = props.location;
+  if (y === 0) {
+    return null;
+  } else {
+    return (
+      <div
+        onClick={e => {
+          props.onLocationChange([x, y - 1]);
+        }}
+        style={{
+          ...arrowStyles,
+          top: 0,
+          left: "15%",
+          width: "70%",
+          height: "15%"
+        }}
+      >
+        <i className="fas fa-arrow-circle-up"></i>
+      </div>
+    );
+  }
+}
+function DownArrow(props) {
+  const [x, y] = props.location;
+  if (y === cells.y - 1) {
+    return null;
+  } else {
+    return (
+      <div
+        onClick={e => {
+          props.onLocationChange([x, y + 1]);
+        }}
+        style={{
+          ...arrowStyles,
+          bottom: 0,
+          left: "15%",
+          width: "70%",
+          height: "15%"
+        }}
+      >
+        <i className="fas fa-arrow-circle-down"></i>
+      </div>
+    );
+  }
+}
+function RightArrow(props) {
+  const [x, y] = props.location;
+  if (x === cells.x - 1) {
+    return null;
+  } else {
+    return (
+      <div
+        onClick={e => {
+          props.onLocationChange([x + 1, y]);
+        }}
+        style={{
+          ...arrowStyles,
+          top: "15%",
+          right: 0,
+          width: "15%",
+          height: "70%"
+        }}
+      >
+        <i className="fas fa-arrow-circle-right"></i>
+      </div>
+    );
+  }
+}
+function LeftArrow(props) {
+  const [x, y] = props.location;
+  if (x === 0) {
+    return null;
+  } else {
+    return (
+      <div
+        onClick={e => {
+          props.onLocationChange([x - 1, y]);
+        }}
+        style={{
+          ...arrowStyles,
+          top: "15%",
+          left: 0,
+          width: "15%",
+          height: "70%"
+        }}
+      >
+        <i className="fas fa-arrow-circle-left"></i>
+      </div>
+    );
+  }
 }
 
 function PosterPositioner(props) {
@@ -106,6 +243,7 @@ function AdjacentSquareDimmer() {
       <div
         style={{
           position: "absolute",
+          left: 0,
           bottom: 0,
           width: "100%",
           height: "15%",

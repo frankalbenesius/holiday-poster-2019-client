150f581bf16e49a4d7cd8b49f97a5411ca0d2c61
frankalbenesius <frankalbenesius@gmail.com>
2020-05-05 17:16:28 -0500

    Use secure api key

--- src/components/DataProvider.js
+++ src/components/DataProvider.js
@@ -3,13 +3,9 @@ import "firebase/firestore";
 import { useEffect, useState } from "react";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyC2EODpeg9hRgQ-iCzdixXg5eu6mM-Bk-0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "holiday-poster-2019.firebaseapp.com",
-  databaseURL: "https://holiday-poster-2019.firebaseio.com",
   projectId: "holiday-poster-2019",
-  storageBucket: "holiday-poster-2019.appspot.com",
-  messagingSenderId: "69143548505",
-  appId: "1:69143548505:web:4841837d218bf1ba4f9748",
 });
 var db = firebase.firestore();
 
